fb5806ebf1f2a11f1d4dba22ce34f4e3a590d976
Therealamoah <collinsamoah594@gmail.com>
2025-07-30 21:07:41 +0000

    done

--- index.js
+++ index.js
@@ -36,39 +36,46 @@
 // console.log(z2);
 
 // arrays function
-const percentage = (score, totalScore) => {
-    let totalPercentage = (score / totalScore) * 100;
+// const percentage = (score, totalScore) => {
+//     let totalPercentage = (score / totalScore) * 100;
 
-    if (totalPercentage>= 80) {
-    return "A";
-     }else if (totalPercentage >= 70)  {
-        console.log("B");
-        }else if (totalPercentage >= 60)  {
-        console.log("C");
-        }else if (totalPercentage >= 50)  {
-        console.log("D");
-        }else if (totalPercentage >= 40)  {
-        console.log("E");
-     }
+//     if (totalPercentage>= 80) {
+//     return "A";
+//      }else if (totalPercentage >= 70)  {
+//         console.log("B");
+//         }else if (totalPercentage >= 60)  {
+//         console.log("C");
+//         }else if (totalPercentage >= 50)  {
+//         console.log("D");
+//         }else if (totalPercentage >= 40)  {
+//         console.log("E");
+//      }
 
-     return totalPercentage;
-};
+//      return totalPercentage;
+// };
 
-console.log(percentage(80, 100));
-;
+// console.log(percentage(80, 100));
+// ;
 
-// Check if even or odd multiple of 5
-function checker(number) {
-    if (number % 2 === 0 && number % 5 === 0) {
-        return "Is even and multiple of 5";
-    } else if (number % 2 === 0 && number % 5 !== 0) {
-        return "number is even and not a multiple of 5";
-    } else {
-        return "odd";
-    }
-}
+// // Check if even or odd multiple of 5
+// function checker(number) {
+//     if (number % 2 === 0 && number % 5 === 0) {
+//         return "Is even and multiple of 5";
+//     } else if (number % 2 === 0 && number % 5 !== 0) {
+//         return "number is even and not a multiple of 5";
+//     } else {
+//         return "odd";
+//     }
+// }
 
-console.log(checker(59));
+// console.log(checker(59));
+
+// // Exercise
+// function numbers(num1, num2, num3) {
+//     const num = num1 + num2 + num3;
+//     return num;
+// }
+// console.log(numbers(10, 20, 30));
 
 
 
