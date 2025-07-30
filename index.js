f6f4a3099dc830eb06a6c5ab6078653989fe0da9
Therealamoah <collinsamoah594@gmail.com>
2025-07-30 13:32:45 +0000

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+// operations
+// let z = 10;
+// let i = 20;
+// console.log(z + i); 
+
+// let a = 30;
+// let b = 40;
+// console.log(a - b);
+
+// let c = 50;
+// let d = 60;
+// console.log(c * d);
+
+// let g = 70;
+// let j = 80;
+// console.log(g / j);
+
+// let a3 = 90;
+// let a4 = 100;        
+// console.log(a3 % a4);
+
+// // console.log
+// let h = "Hello, World!";
+// console.log(h);
+
+// let p1 = "JavaScript is fun!";
+// console.log(p1);
+
+// let y4 = "I love coding!";
+// console.log(y4);
+
+// let z1 = "Let's learn together!";
+// console.log(z1);
+
+// let z2 = "Happy coding!";
+// console.log(z2);
+
+// functions
+
+
+
